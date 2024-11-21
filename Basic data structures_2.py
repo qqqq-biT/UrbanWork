f4dd1c2e14310b9b187d3e2000b29813687500f5
Absolute <187764974+qqqq-biT@users.noreply.github.com>
2024-11-21 09:31:49 +0300

    Update Basic data structures_2.py

--- Basic data structures_2.py
+++ Basic data structures_2.py
@@ -3,11 +3,11 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 s = sorted(list(students))
 
-total = (float(sum(grades[0])) / (len(grades[0])))
-total_1 = (float(sum(grades[1])) / (len(grades[1])))
-total_2 = (float(sum(grades[2])) / (len(grades[2])))
-total_3 = (float(sum(grades[3])) / (len(grades[3])))
-total_4 = (float(sum(grades[4])) / (len(grades[4])))
+g = (float(sum(grades[0])) / (len(grades[0])))
+g_1 = (float(sum(grades[1])) / (len(grades[1])))
+g_2 = (float(sum(grades[2])) / (len(grades[2])))
+g_3 = (float(sum(grades[3])) / (len(grades[3])))
+g_4 = (float(sum(grades[4])) / (len(grades[4])))
 
-a = {total: s[0], s[1]: total_1, s[2]: total_2, s[3]: total_3, s[4]: total_4}
+a = {g: s[0], s[1]: g_1, s[2]: g_2, s[3]: g_3, s[4]: g_4}
 print(a)
