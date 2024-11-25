9e23e701ca4e47d9c7fab2c3ef9342935c42774a
Absolute <187764974+qqqq-biT@users.noreply.github.com>
2024-11-25 20:02:29 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,1 +1,15 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
+index = 0
+
+while index < len(my_list):
+    qbit = my_list[index]
+    if qbit < 0:
+        break
+    if qbit == 0:
+        index += 1
+        continue
+
+    print(qbit)
+
+    index += 1
